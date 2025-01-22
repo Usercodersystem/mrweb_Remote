19f4c8c47b6793fcab8c4458b062a8a70b78e09b
oneapp <70068765+Usercodersystem@users.noreply.github.com>
2025-01-22 21:09:22 +0330

    اضافه شدن تابع ai

--- mrweb.py
+++ mrweb.py
@@ -1,5 +1,10 @@
 from requests import get,post
 from json import loads
+from urllib.parse import urlencode
 
 def test():
   return "mrweb remote library"
+
+def ai(text,urlencode=True):
+  txt=urlencode({"text":text})
+  return get(f"http://api4dev.ir/ai/?{txt}").text
